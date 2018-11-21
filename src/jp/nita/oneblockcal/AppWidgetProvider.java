5a1cac8dcc6d2af882a00af9bd088113106bb43b
kenta.hatano <kenta.hatano@s-cubism.jp>
2018-11-21 20:08:30 +0900

    時差考慮の修正

--- src/jp/nita/oneblockcal/AppWidgetProvider.java
+++ src/jp/nita/oneblockcal/AppWidgetProvider.java
@@ -13,7 +13,7 @@ public class AppWidgetProvider extends android.appwidget.AppWidgetProvider {
 
 	static void updateWidget(Context context, AppWidgetManager manager, int id) {
 		Date date = new Date();
-		long time = date.getTime() + date.getTimezoneOffset() * 60 * 1000;
+		long time = date.getTime() - date.getTimezoneOffset() * 60 * 1000;
 		
 		RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.main);
 		views.setTextViewText(R.id.textview_center_large, Statics.getHeavenlyStemIdeograph(context, time));
